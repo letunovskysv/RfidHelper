d459a4ddff3f84fa7a5250fbd721456bde5d5cc9
letunovskysv <62968494+letunovskysv@users.noreply.github.com>
2023-05-30 10:01:02 +0300

    +docs

---
+++ SmartMinex.Core/Modules/Monitor/RfidMonitorService.cs
@@ -0,0 +1,16 @@
+﻿//--------------------------------------------------------------------------------------------------
+// (C) 2023-2023 UralTehIS, LLC. UTIS Smart System Platform. Version 2.0. All rights reserved.
+// Описание: RfidMonitorService – Блок опроса меток (БОм).
+//--------------------------------------------------------------------------------------------------
+namespace SmartMinex.Rfid.Modules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class RfidMonitorService
+    {
+    }
+}
