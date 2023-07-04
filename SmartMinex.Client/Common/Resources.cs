205137c4e4bc76dc326b4c9491c8a7ba1c66e444
letunovskysv <62968494+letunovskysv@users.noreply.github.com>
2023-07-04 21:26:06 +0300

    +help

--- SmartMinex.Client/Common/Resources.cs
+++ SmartMinex.Client/Common/Resources.cs
@@ -2,10 +2,13 @@
 // (C) 2023-2023 UralTehIS, LLC. UTIS Smart System Platform. Version 2.0. All rights reserved.
 // Описание: Различные ресурсы.
 //--------------------------------------------------------------------------------------------------
+using System.Reflection;
+
 namespace SmartMinex.Web
 {
     public class Resources
     {
-        public static string Title = "Блок опроса меток";
+        public static Version Version { get; } = Assembly.GetEntryAssembly().GetName().Version;
+        public static string Title { get; } = "Блок опроса меток " + Version.ToString(2);
     }
 }
